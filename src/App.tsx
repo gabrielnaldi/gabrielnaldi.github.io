24fca9c4b3fa23e371bf10ab97852ef47db9b8a8
Gabriel <gabrielnaldi.castro@gmail.com>
2023-01-26 11:49:15 -0300

    update: renamed home to homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Home } from './pages/Home';
+import { HomePage } from './pages/HomePage';
 import './styles/global.css';
 
 function App() {
 
   return (
     <div className="bg-background w-screen h-screen">
-      <Home />
+      <HomePage />
     </div>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import { Content } from "../components/Content";
 import { Header } from "../components/Header";
 
-export function Home() {
+export function HomePage() {
     return (
         <div className="max-w-5xl border-2 border-red-500 mx-auto">
             <Header />
+            <Content />
         </div>
     )
 }
