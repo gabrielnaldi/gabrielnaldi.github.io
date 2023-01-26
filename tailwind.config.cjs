e9ca6aa557021c0e6b46a55e9358b861a016f791
Gabriel <gabrielnaldi.castro@gmail.com>
2023-01-26 14:07:00 -0300

    update: configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,12 @@ module.exports = {
       },
       colors: {
         background: '#10101a',
-        main: '#2fbf71'
+        main: '#2fbf71',
+        description: '#ffffffcc'
+      },
+      boxShadow: {
+        btnMain: '#fff 4px 4px',
+        btnSecondary: '#2fbf71 4px 4px',
       }
     },
   },
