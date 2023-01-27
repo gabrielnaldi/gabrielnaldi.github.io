e352062afc100a0da2f1c57e19918a350348c724
Gabriel <gabrielnaldi.castro@gmail.com>
2023-01-27 09:15:25 -0300

    config: react-reveal types

---
+++ src/@types/react-reveal.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-reveal'
+declare module 'react-reveal/Fade'
+declare module 'react-reveal/Flip'
+declare module 'react-reveal/Slide'
+declare module 'react-reveal/Zoom'
+declare module 'react-reveal/Bounce'
