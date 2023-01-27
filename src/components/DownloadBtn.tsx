709e2fbf046e520f430978c770688cd6c11cdd58
Gabriel <gabrielnaldi.castro@gmail.com>
2023-01-27 09:17:00 -0300

    update: download btn styles

--- src/components/DownloadBtn.tsx
+++ src/components/DownloadBtn.tsx
@@ -4,7 +4,8 @@ interface DownloadBtnProps {
 
 export function DownloadBtn({ title }: DownloadBtnProps) {
     return (
-        <button className="bg-main text-white px-5 py-2 border border-transparent text-base font-normal tracking-widest duration-300 shadow-btnMain shadow-white hover:bg-white hover:text-main hover:shadow-btnSecondary">
+        <button className="bg-main text-white px-5 py-2 border border-transparent text-base 
+        font-medium tracking-[2px] duration-300 shadow-btnMain shadow-white hover:bg-white hover:text-main hover:shadow-btnSecondary">
             {title}
         </button>
     )
