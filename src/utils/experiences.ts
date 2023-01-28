655a065bb0614166439ca202ab9df1d0c99204b0
Gabriel <gabrielnaldi.castro@gmail.com>
2023-01-28 17:28:03 -0300

    add frontend experiences

---
+++ src/utils/experiences.ts
@@ -0,0 +1,53 @@
+import { ExperienceData, Level } from "./interfaces";
+
+export const experiencesList: Array<ExperienceData> = [
+    {
+        title: 'HTML',
+        level: 'Experienced'
+    },
+    {
+        title: 'CSS',
+        level: 'Experienced',
+    },
+    {
+        title: 'JavaScript',
+        level: 'Experienced'
+    },
+    {
+        title: 'React',
+        level: 'Experienced'
+    },
+    {
+        title: 'ViteJs',
+        level: 'Experienced'
+    },
+    {
+        title: 'Next.js',
+        level: 'Experienced'
+    },
+    {
+        title: 'React Native',
+        level: 'Intermediate'
+    },
+    {
+        title: 'Flutter',
+        level: 'Intermediate'
+    },
+    {
+        title: 'Bootstrap',
+        level: 'Experienced'
+    },
+
+    {
+        title: 'Tailwind',
+        level: 'Experienced'
+    },
+    {
+        title: 'Styled Comp.',
+        level: 'Experienced'
+    },
+    {
+        title: 'Sass',
+        level: 'Experienced'
+    },
+]
