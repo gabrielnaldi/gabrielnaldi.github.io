57761bd609557aad65d3528bd4d110270b615675
Gabriel <gabrielnaldi.castro@gmail.com>
2023-01-29 11:50:46 -0300

    add fade effect

--- src/components/Sections/Experience.tsx
+++ src/components/Sections/Experience.tsx
@@ -2,6 +2,8 @@ import { experiencesList, frontendExperiencesList } from "../../utils/experience
 import { SectionHeader } from "../SectionHeader";
 import { ExperienceElement } from "./AboutMe/ExperienceElement";
 
+import Fade from 'react-reveal/Fade';
+
 export function Experience() {
     return (
         <section id="experience"
@@ -13,35 +15,39 @@ export function Experience() {
                 className="container do front e back grid grid-cols-2 gap-10"
             >
 
-                <div className="bg-card rounded-3xl flex flex-col gap-8 items-center py-6 px-4 border border-transparent hover:border-main duration-300">
-                    <div className="">
-                        <h2 className="text-main text-lg font-semibold -tracking-wide">
-                            Frontend Development
-                        </h2>
+                <Fade left>
+                    <div className="bg-card rounded-3xl flex flex-col gap-8 items-center py-6 px-4 border border-transparent hover:border-main duration-300">
+                        <div className="">
+                            <h2 className="text-main text-lg font-semibold -tracking-wide">
+                                Frontend Development
+                            </h2>
+                        </div>
+                        <div className="grid grid-cols-2 gap-x-10 gap-y-6 max-h-[250px] overflow-y-auto over">
+                            {experiencesList.map(experience => {
+                                return (
+                                    <ExperienceElement key={experience.title} title={experience.title} level={experience.level} />
+                                )
+                            })}
+                        </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-x-10 gap-y-6 max-h-[250px] overflow-y-auto over">
-                        {experiencesList.map(experience => {
-                            return (
-                                <ExperienceElement key={experience.title} title={experience.title} level={experience.level} />
-                            )
-                        })}
-                    </div>
-                </div>
+                </Fade>
 
-                <div className="bg-card rounded-3xl flex flex-col gap-8 items-center py-6 px-4 border border-transparent hover:border-main duration-300">
-                    <div className="">
-                        <h2 className="text-main text-lg font-semibold -tracking-wide">
-                            Backend Development
-                        </h2>
+                <Fade right>
+                    <div className="bg-card rounded-3xl flex flex-col gap-8 items-center py-6 px-4 border border-transparent hover:border-main duration-300">
+                        <div className="">
+                            <h2 className="text-main text-lg font-semibold -tracking-wide">
+                                Backend Development
+                            </h2>
+                        </div>
+                        <div className="grid grid-cols-2 gap-x-10 gap-y-6 max-h-[250px] overflow-y-auto over">
+                            {frontendExperiencesList.map(experience => {
+                                return (
+                                    <ExperienceElement key={experience.title} title={experience.title} level={experience.level} />
+                                )
+                            })}
+                        </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-x-10 gap-y-6 max-h-[250px] overflow-y-auto over">
-                        {frontendExperiencesList.map(experience => {
-                            return (
-                                <ExperienceElement key={experience.title} title={experience.title} level={experience.level} />
-                            )
-                        })}
-                    </div>
-                </div>
+                </Fade>
             </div>
         </section >
     )
