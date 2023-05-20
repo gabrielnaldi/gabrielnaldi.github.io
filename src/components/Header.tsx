ecc38d3a9e32d8c869934519453310275105d50b
Gabriel <gabrielnaldi.castro@gmail.com>
2023-05-20 12:36:15 -0300

    add mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
-import { Navbar } from "./Navbar";
+import { useState } from 'react'
+import { AiFillCloseCircle, AiOutlineMenu } from 'react-icons/ai'
 
 export function Header() {
     return (
-        <header className="w-full flex justify-between items-center fixed max-w-5xl z-50 bg-background px-4 py-6">
-            <span className="text-white text-3xl font-semibold">
-                Portfo
-                <span className="text-main">
-                    lio
+        <header className="z-10 fixed w-full bg-containerColor">
+            <div className="px-6 py-4 flex justify-between items-center">
+                <span className='text-2xl font-semibold'>
+                    Gabriel <span className='text-firstColor'>Naldi</span>
                 </span>
-            </span>
-            <Navbar />
+
+                <span className='hover:cursor-pointer'>
+                    <AiOutlineMenu size={24} />
+                </span>
+            </div>
         </header>
     )
 }
