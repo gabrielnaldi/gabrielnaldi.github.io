83e23a0f2be625d3aa8e9fbcc0fd75935b0e71ef
Gabriel <gabrielnaldi.castro@gmail.com>
2023-05-20 12:35:28 -0300

    removed content and update layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
-import { Content } from "../components/Content";
 import { Header } from "../components/Header";
 
 export function HomePage() {
     return (
-        <div className="max-w-5xl mx-auto w-full">
+        <div className="w-full">
             <Header />
-            <Content />
         </div>
     )
 }
