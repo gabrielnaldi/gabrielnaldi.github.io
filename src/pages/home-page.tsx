1f993455e564cca21fe5424119553cef4c30ab77
Gabriel <gabrielnaldi.castro@gmail.com>
2023-05-21 11:39:34 -0300

    add padding

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,9 +1,11 @@
+import { Content } from "../components/content";
 import { Header } from "../components/header";
 
 export function HomePage() {
     return (
-        <div className="w-full">
+        <div className="w-full pt-16">
             <Header />
+            <Content />
         </div>
     )
 }
