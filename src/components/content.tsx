f49fa61c87daa8dfacd1923552fd0796bf5bcfaa
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-08 19:13:07 -0300

    create home mobile

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,50 +1,9 @@
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { HomeSection } from "./sections/home";
 
 export function Content() {
     return (
-        <div className="max-w-8xl mx-auto px-8">
-            <section id="home" className="pb-16">
-                <div className="pt-12">
-                    <div className="text-center mb-8">
-                        <h3 className="text-xl font-semibold mb-2">
-                            Hello! {" "}
-                            <span className="text-firstColorAlt">I am</span>
-                        </h3>
-                        <h1 className="text-white text-5xl font-bold mb-2">Gabriel {' '}
-                            <span className="text-white">
-                                Naldi
-                            </span>
-                        </h1>
-                        <h3 className="text-lg font-medium mb-5">
-                            Full-stack {' '}
-                            <span className="text-firstColor">Developer</span>
-                        </h3>
-                        <p className="text-textColor text-sm leading-6">
-                            With knowledge in development of modern Websites, Services, mobile Apps and APIs. I offer the best projects resulting in {' '}
-                            <span className="text-textColor text-sm leading-6 whitespace-nowrap">quality work.</span>
-                        </p>
-                    </div>
-                    <div className="text-center mb-12">
-                        <div className="mb-12">
-                            <a href="#contact" className="font-medium px-4 py-3 bg-firstColor rounded-md border-[1.3px] border-firstColor hover:bg-white hover:text-firstColor transition-colors duration-200">Let's talk</a>
-                        </div>
-                        <div className="flex justify-center items-center gap-4">
-                            <a href="https://github.com/gabrielnaldi" target="_blank" className="h-8 w-8 bg-firstColorAlt flex items-center justify-center clip-path-bg hover:-translate-y-1 transition-transform duration-200">
-                                <AiFillGithub size={20} color="#fff" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/gabriel-naldi-69b534193/" target="_blank" className="h-8 w-8 bg-firstColorAlt flex items-center justify-center clip-path-bg hover:-translate-y-1 transition-transform duration-200">
-                                <AiFillLinkedin size={20} color="#fff" />
-                            </a>
-                            <a href="https://www.instagram.com/gnaldi_/" target="_blank" className="h-8 w-8 bg-firstColorAlt flex items-center justify-center clip-path-bg hover:-translate-y-1 transition-transform duration-200">
-                                <AiFillInstagram size={20} color="#fff" />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="p-8 pt-0 flex justify-center items-center relative ">
-                    <img src="/profile-pic-placeholder2.png" alt="" className="z-10" width={200} />
-                </div>
-            </section>
+        <div className="max-w-8xl mx-auto px-8 py-12 flex flex-col">
+            <HomeSection />
         </div>
     )
 }
---
+++ src/components/sections/home.tsx
@@ -0,0 +1,37 @@
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+
+export function HomeSection() {
+    return (
+        <section id="home">
+            <div className="container">
+                <div className="content flex flex-col gap-8">
+                    <div className="first border border-red-200 flex flex-col items-center justify-center">
+                        <h3 className="text-xl font-semibold mb-2">
+                            Hello! {" "}
+                            <span className="text-firstColorAlt">I am</span>
+                        </h3>
+                        <h1 className="text-white text-center text-5xl font-bold mb-2">Gabriel {' '}
+                            <span className="text-white">
+                                Naldi
+                            </span>
+                        </h1>
+                        <h3 className="text-lg text-center font-medium mb-5">
+                            Full-stack {' '}
+                            <span className="text-firstColor">Developer</span>
+                        </h3>
+                        <p className="text-textColor text-center text-sm leading-6">
+                            With knowledge in development of modern Websites, Services, mobile Apps and APIs. I offer the best projects resulting in {' '}
+                            <span className="text-textColor font-bold text-sm leading-6 whitespace-nowrap">quality work.</span>
+                        </p>
+                    </div>
+                    <div className="second border border-red-200 flex justify-center">
+                        <div className="img-container max-w-[280px] relative">
+                            <img src="./profile-pic.png" className="" alt="" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
