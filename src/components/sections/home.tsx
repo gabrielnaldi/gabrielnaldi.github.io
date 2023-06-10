1f17a7cf7d37deafc983bbdddf77e0cc44f0df24
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-10 15:02:31 -0300

    create social media area

--- src/components/sections/home.tsx
+++ src/components/sections/home.tsx
@@ -5,7 +5,7 @@ export function HomeSection() {
         <section id="home">
             <div className="container">
                 <div className="content flex flex-col gap-8">
-                    <div className="first border border-red-200 flex flex-col items-center justify-center gap-8">
+                    <div className="first border border-red-200 flex flex-col items-center justify-center gap-12">
                         <div className="texts">
                             <h3 className="text-xl text-center font-semibold mb-2">
                                 Hello! {" "}
@@ -28,7 +28,17 @@ export function HomeSection() {
                         <div>
                             <a href="#contact" className="border-[1.4px] border-firstColor py-3 px-5 bg-firstColor rounded-md font-semibold hover:bg-white hover:text-firstColor transition-colors duration-300">Let's talk</a>
                         </div>
-
+                        <div className="links flex gap-4">
+                            <a href="https://github.com/gabrielnaldi" target="_blank" className="flex items-center justify-center bg-firstColorAlt p-1 clip-path-bg hover:-translate-y-1 transition-transform duration-300">
+                                <AiFillGithub size={20} />
+                            </a>
+                            <a href="https://www.instagram.com/gnaldi_/" target="_blank" className="flex items-center justify-center bg-firstColorAlt p-1 clip-path-bg hover:-translate-y-1 transition-transform duration-300">
+                                <AiFillInstagram size={20} />
+                            </a>
+                            <a href="https://www.linkedin.com/in/gabriel-naldi-69b534193/" target='_blank' className="flex items-center justify-center bg-firstColorAlt p-1 clip-path-bg hover:-translate-y-1 transition-transform duration-300">
+                                <AiFillLinkedin size={20} />
+                            </a>
+                        </div>
                     </div>
                     <div className="second border border-red-200 flex justify-center">
                         <div className="img-container max-w-[280px] relative">
