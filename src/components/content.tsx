82d8e8fce9514e5c49f8ac48315fb0d81e65e1ca
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-11 17:59:19 -0300

    fix content size

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -2,7 +2,7 @@ import { HomeSection } from "./sections/home";
 
 export function Content() {
     return (
-        <div className="max-w-8xl mx-auto px-8 py-12 flex flex-col">
+        <div className="px-8 py-12 flex flex-col">
             <HomeSection />
         </div>
     )
