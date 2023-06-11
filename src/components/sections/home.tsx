ae2023550c8f4d8a493e566e0b581c8420da1638
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-11 17:59:32 -0300

    update home styles

--- src/components/sections/home.tsx
+++ src/components/sections/home.tsx
@@ -3,48 +3,44 @@ import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 export function HomeSection() {
     return (
         <section id="home">
-            <div className="container">
-                <div className="content flex flex-col gap-8">
-                    <div className="first border border-red-200 flex flex-col items-center justify-center gap-12">
-                        <div className="texts">
-                            <h3 className="text-xl text-center font-semibold mb-2">
-                                Hello! {" "}
-                                <span className="text-firstColorAlt">I am</span>
+            <div className="">
+                <div className="grid grid-cols-1 gap-16 md:grid-cols-2 md:justify-center">
+                    <div className="flex flex-col gap-12">
+                        <div className="pb-8 relative after-border">
+                            <h3 className="text-2xl text-center font-semibold mb-2 md:text-left">
+                                Hello! I'm
                             </h3>
-                            <h1 className="text-white text-center text-5xl font-bold mb-2">Gabriel {' '}
+                            <h1 className="text-white text-center text-6xl font-bold mb-2 md:text-left">Gabriel {' '}
                                 <span className="text-white">
                                     Naldi
                                 </span>
                             </h1>
-                            <h3 className="text-lg text-center font-medium mb-5">
+                            <h3 className="text-lg text-center font-medium mb-5 md:text-left">
                                 Full-stack {' '}
                                 <span className="text-firstColor">Developer</span>
                             </h3>
-                            <p className="text-textColor text-center text-sm leading-6">
+                            <p className="text-textColor text-center text-sm leading-6 md:text-left">
                                 With knowledge in development of modern Websites, Services, mobile Apps and APIs. I offer the best projects resulting in {' '}
                                 <span className="text-textColor font-bold text-sm leading-6 whitespace-nowrap">quality work.</span>
                             </p>
                         </div>
-                        <div>
+                        <div className="flex justify-center w-full md:justify-start">
                             <a href="#contact" className="border-[1.4px] border-firstColor py-3 px-5 bg-firstColor rounded-md font-semibold hover:bg-white hover:text-firstColor transition-colors duration-300">Let's talk</a>
                         </div>
-                        <div className="links flex gap-4">
-                            <a href="https://github.com/gabrielnaldi" target="_blank" className="flex items-center justify-center bg-firstColorAlt p-1 clip-path-bg hover:-translate-y-1 transition-transform duration-300">
-                                <AiFillGithub size={20} />
+                        <div className="links flex gap-4 justify-center items-center md:justify-start">
+                            <a href="https://github.com/gabrielnaldi" target="_blank" className="flex items-center justify-center bg-firstColorAlt p-1 rounded-full hover:-translate-y-1 transition-transform duration-300">
+                                <AiFillGithub size={22} />
                             </a>
-                            <a href="https://www.instagram.com/gnaldi_/" target="_blank" className="flex items-center justify-center bg-firstColorAlt p-1 clip-path-bg hover:-translate-y-1 transition-transform duration-300">
-                                <AiFillInstagram size={20} />
+                            <a href="https://www.instagram.com/gnaldi_/" target="_blank" className="flex items-center justify-center bg-firstColorAlt p-1 rounded-full hover:-translate-y-1 transition-transform duration-300">
+                                <AiFillInstagram size={22} />
                             </a>
-                            <a href="https://www.linkedin.com/in/gabriel-naldi-69b534193/" target='_blank' className="flex items-center justify-center bg-firstColorAlt p-1 clip-path-bg hover:-translate-y-1 transition-transform duration-300">
-                                <AiFillLinkedin size={20} />
+                            <a href="https://www.linkedin.com/in/gabriel-naldi-69b534193/" target='_blank' className="flex items-center justify-center bg-firstColorAlt p-1 rounded-full hover:-translate-y-1 transition-transform duration-300">
+                                <AiFillLinkedin size={22} />
                             </a>
                         </div>
                     </div>
-                    <div className="second border border-red-200 flex justify-center">
-                        <div className="img-container max-w-[280px] relative">
-                            <img src="./profile-pic.png" className="" alt="" />
-                        </div>
-
+                    <div className="flex justify-center items-center">
+                        <img src="./profile-pic.png" className="w-full max-w-sm md:max-w-md" alt="" />
                     </div>
                 </div>
             </div>
