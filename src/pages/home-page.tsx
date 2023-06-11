a313b3bcb5984c6ff37ce6e91ac0432536a57e7d
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-11 17:58:57 -0300

    fix home container

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -4,8 +4,10 @@ import { Header } from "../components/header";
 export function HomePage() {
     return (
         <div className="w-full pt-16">
-            <Header />
-            <Content />
+            <div className="max-w-6xl ml-auto mr-auto">
+                <Header />
+                <Content />
+            </div>
         </div>
     )
 }
