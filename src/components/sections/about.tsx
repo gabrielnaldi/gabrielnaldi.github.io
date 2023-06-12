bdbdb5a34ec42cc1a3e539e3727d487465924cf0
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-12 19:54:33 -0300

    fix section title

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -7,7 +7,7 @@ export const AboutSection: React.FC = () => {
     return (
         <section id='about' className="mt-16">
             <div>
-                <SectionTitle title='About me' subtitle='Intro' />
+                <SectionTitle title='About me' subtitle='My' postSubtitle='Intro' />
                 <ul className="flex flex-wrap justify-center items-center gap-6 mb-8 max-w-2xl mx-auto">
                     <li className='bg-containerColor flex flex-col items-center justify-center gap-1 w-full max-w-[180px] p-4 rounded-lg border-[1.4px] border-containerColor hover:cursor-default hover:border-firstColor hover:brightness-110 transition-all duration-300'>
                         <TfiMedallAlt size={32} fill='#9336B4' />
--- src/components/sections/section-title.tsx
+++ src/components/sections/section-title.tsx
@@ -1,12 +1,13 @@
 interface Props {
     title: string;
     subtitle: string;
+    postSubtitle: string;
 }
 
-export const SectionTitle: React.FC<Props> = ({ title, subtitle }) => {
+export const SectionTitle: React.FC<Props> = ({ title, subtitle, postSubtitle }) => {
     return (
         <div className="mb-16">
-            <h4 className="text-base font-semibold text-center">My <span className="text-firstColor">{subtitle}</span></h4>
+            <h4 className="text-base font-semibold text-center">{subtitle} <span className="text-firstColor">{postSubtitle}</span></h4>
             <h2 className="text-2xl font-extrabold text-center">{title}</h2>
         </div>
     )
