6fbd14895f7a5451626c8edd6c0bc9e0e6e368fa
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-12 18:55:33 -0300

    create about section

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,9 +1,11 @@
+import { AboutSection } from "./sections/about";
 import { HomeSection } from "./sections/home";
 
 export function Content() {
     return (
         <div className="px-8 py-12 flex flex-col">
             <HomeSection />
+            <AboutSection />
         </div>
     )
 }
---
+++ src/components/sections/about.tsx
@@ -0,0 +1,44 @@
+import { TfiMedallAlt } from 'react-icons/tfi'
+import { VscFolderActive } from 'react-icons/vsc'
+import { AiOutlineUsergroupAdd } from 'react-icons/ai'
+
+export const AboutSection: React.FC = () => {
+    return (
+        <section id='about' className="mt-16">
+            <div>
+                <div className="mb-16">
+                    <h4 className="text-base font-semibold text-center">My <span className="text-firstColor">Intro</span></h4>
+                    <h2 className="text-2xl font-extrabold text-center">About me</h2>
+                </div>
+
+                <ul className="flex flex-wrap justify-center items-center gap-6 mb-8 max-w-2xl mx-auto">
+                    <li className='bg-containerColor flex flex-col items-center justify-center gap-1 w-full max-w-[180px] p-4 rounded-lg border-[1.4px] border-containerColor hover:cursor-default hover:border-firstColor hover:brightness-110 transition-all duration-300'>
+                        <TfiMedallAlt size={32} fill='#9336B4' />
+                        <h2 className='font-bold'>Experience</h2>
+                        <p className='text-textColor'>4+ Years working</p>
+                    </li>
+                    <li className='bg-containerColor flex flex-col items-center justify-center gap-1 w-full max-w-[180px] p-4 rounded-lg border-[1.4px] border-containerColor hover:cursor-default hover:border-firstColor hover:brightness-110 transition-all duration-300'>
+                        <VscFolderActive size={32} fill='#9336B4' />
+                        <h2 className='font-bold'>Projects</h2>
+                        <p className='text-textColor'>30+ Completed</p>
+                    </li>
+                    <li className='bg-containerColor flex flex-col items-center justify-center gap-1 w-full max-w-[180px] p-4 rounded-lg border-[1.4px] border-containerColor hover:cursor-default hover:border-firstColor hover:brightness-110 transition-all duration-300'>
+                        <AiOutlineUsergroupAdd size={32} fill='#9336B4' />
+                        <h2 className='font-bold'>Clients</h2>
+                        <p className='text-textColor'>20+ National</p>
+                    </li>
+                </ul>
+                <div className="flex justify-center mb-12 max-w-2xl mx-auto">
+                    <p className="text-textColor text-center text-sm leading-6 md:text-left">
+                        I like to describe myself as ambitious and passionate person when it comes to work.
+                        I love what I do and I'm always looking for opportunities to grow.
+                        That's why I constantly challenge myself and create goals. For example, in my last job, I learned Flutter to improve my technical abilities and soon I was able to lead the company Mobile Team to work with this new technology in several new projects.
+                    </p>
+                </div>
+                <div className="flex justify-center">
+                    <a href="" className="py-4 px-6 bg-firstColor border-[1.4px] border-firstColor rounded-lg font-bold transition-colors duration-300 hover:bg-white hover:text-firstColor">Download CV</a>
+                </div>
+            </div>
+        </section>
+    )
+}
