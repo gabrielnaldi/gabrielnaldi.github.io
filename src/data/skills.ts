40f3c66f0488c06e2afac08d6275033ec3a50263
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-16 09:32:10 -0300

    create skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,38 @@
+interface ISkill {
+    name: string;
+}
+export const frontEndSkills: ISkill[] = [
+    {
+        name: 'HTML & CSS'
+    },
+    {
+        name: 'JavaScript'
+    },
+    {
+        name: 'React'
+    },
+    {
+        name: 'Next.js'
+    },
+    {
+        name: 'Vite Js'
+    },
+    {
+        name: 'React Native'
+    },
+    {
+        name: 'Bootstrap'
+    },
+    {
+        name: 'Styled Comp.'
+    },
+    {
+        name: 'Tailwindcss'
+    },
+    {
+        name: 'Sass'
+    },
+    {
+        name: 'Flutter'
+    },
+]
