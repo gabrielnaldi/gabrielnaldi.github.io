9c5891e4a4b13ba7cc1f35bd679e0d5702627818
Gabriel <gabrielnaldi.castro@gmail.com>
2023-06-21 21:13:54 -0300

    update: add grid layout

--- src/components/sections/projects/index.tsx
+++ src/components/sections/projects/index.tsx
@@ -1,13 +1,18 @@
 import { SectionTitle } from "../section-title";
-import { HiExternalLink } from 'react-icons/hi'
 import { ProjectItem } from "./project-item";
 
 export function ProjectsSection() {
     return (
-        <section id="projects" className="min-h-section py-16 mb-16 border">
-            <div className="border">
+        <section id="projects" className="min-h-section py-16 mb-16">
+            <div className="">
                 <SectionTitle title="Recent Projects" subtitle="My" postSubtitle="Jobs" />
-                <ul className="border p-4">
+                <ul className="p-4 grid grid-cols-auto-300 justify-evenly gap-y-6 gap-x-2">
+                    <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
+                    <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
+                    <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
+                    <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
+                    <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
+                    <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
                     <ProjectItem title="Modern website" subtitle="Web" projectName="project-1" />
                 </ul>
             </div>
--- src/components/sections/projects/project-item.tsx
+++ src/components/sections/projects/project-item.tsx
@@ -8,7 +8,7 @@ interface ProjectItemProps {
 
 export function ProjectItem({ title, subtitle, projectName }: ProjectItemProps) {
     return (
-        <li className={`group min-h-[180px] flex flex-col items-center justify-center gap-2 max-w-[250px] w-full rounded-lg from-firstColor to-transparent bg-cover bg-center bg-no-repeat box-shadow bg-[url:theme('backgroundImage.${projectName}')] hover:bg-[url:_theme('backgroundImage.gradient-to-t'),_theme('backgroundImage.${projectName}')]`}>
+        <li className={`group min-h-[180px] flex flex-col items-center justify-center gap-2 rounded-lg from-firstColor to-transparent bg-cover bg-center bg-no-repeat box-shadow bg-[url:theme('backgroundImage.${projectName}')] hover:bg-[url:_theme('backgroundImage.gradient-to-t'),_theme('backgroundImage.${projectName}')]`}>
             <div className="hidden group-hover:flex justify-center items-center flex-col gap-2">
                 <h3 className="font-semibold text-base">{subtitle}</h3>
                 <h2 className="font-bold text-2xl">{title}</h2>
