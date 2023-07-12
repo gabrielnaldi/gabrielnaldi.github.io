da085bbf0f4e99c5099b7744a527a607340eec58
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-12 20:47:07 -0300

    feat: add toastify lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { Flip, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { HomePage } from './pages/home-page';
 import './styles/global.css';
 
 function App() {
 
   return (
-    <div className="min-h-screen max-w-screen">
-      <HomePage />
-    </div>
+    <>
+      <ToastContainer
+        transition={Flip}
+        theme="colored"
+        autoClose={1500}
+        position="bottom-right"
+      />
+      <div className="min-h-screen max-w-screen">
+        <HomePage />
+      </div>
+    </>
+    
   )
 }
 
