d9ebb2de9f67503741411d24011de3919b86fd66
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-15 18:12:15 -0300

    feat: create projects data file

---
+++ src/data/projects.ts
@@ -0,0 +1,39 @@
+interface Project {
+    title: string;
+    subtitle: string;
+    image: string;
+    link?: string;
+}
+
+export const projectsList: Project[] = [
+    {
+        title: 'Modern website',
+        subtitle: 'Web',
+        image: 'project-1.jpg',
+        link: 'notifications-service'
+    },
+    {
+        title: 'Modern website',
+        subtitle: 'Web',
+        image: 'project-1.jpg',
+        link: 'notifications-service'
+    },
+    {
+        title: 'Modern website',
+        subtitle: 'Web',
+        image: 'project-1.jpg',
+        link: 'notifications-service'
+    },
+    {
+        title: 'Modern website',
+        subtitle: 'Web',
+        image: 'project-1.jpg',
+        link: 'notifications-service'
+    },
+    {
+        title: 'Modern website',
+        subtitle: 'Web',
+        image: 'project-1.jpg',
+        link: 'notifications-service'
+    },
+]
